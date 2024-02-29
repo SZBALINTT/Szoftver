6caad75d3bc57ea9116ebdcaf7d6952b2547fc81
SZBALINTT <szabon.balint@gmail.com>
2024-02-29 10:09:04 +0100

    Szorzótábla meg színezés

---
+++ Gyakorlo1/Form1.cs
@@ -0,0 +1,29 @@
+namespace Gyakorlo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                VillogoGomb uj = new VillogoGomb();
+                uj.Width = 100;
+                uj.Height = 50;
+                uj.Left = i*110;
+                uj.Top = j*55;
+                uj.Text = (i*j).ToString();
+                Controls.Add(uj);
+                }
+
+
+            }
+        }
+    }
+}
