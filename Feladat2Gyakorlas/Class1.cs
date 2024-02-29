ed3a17deef11cea5ffb832eea384508e3c9e2fae
SZBALINTT <szabon.balint@gmail.com>
2024-02-29 10:20:03 +0100

    Színező gomb létrehozva

---
+++ Feladat2Gyakorlas/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat2Gyakorlas
+{
+    internal class SzinezoGomb : Button
+    {
+        public SzinezoGomb()
+        {
+            Width = 20;
+            Height = 20;
+            Click += SzinezoGomb_Click;
+        }
+
+        private void SzinezoGomb_Click(object? sender, EventArgs e)
+        {
+            BackColor = Color.Olive;
+        }
+    }
+}
---
+++ Gyakorlo1/Class2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlo1
+{
+    public class SzinezoGomb:Button
+    {
+        public SzinezoGomb()
+        {
+            Width = 20;
+            Height = 20;
+            Click += SzinezoGomb_Click;
+        }
+
+        private void SzinezoGomb_Click(object? sender, EventArgs e)
+        {
+            BackColor = Color.Olive;
+        }
+    }
+}
